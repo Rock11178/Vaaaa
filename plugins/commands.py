7f09d0f5f998d814cc36d267047bc5c6d1cb7be6
Sohan rajpurohit <132225307+Sohanraz@users.noreply.github.com>
2023-07-18 18:45:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -3,7 +3,6 @@ import logging
 import random
 import asyncio
 from Script import script
-from urllib.parse import quote_plus
 from pyrogram import Client, filters, enums
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import *
